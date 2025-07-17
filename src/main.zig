dd16cd5f0e92d1fbb7c26119b7a47c4b70c4abc1
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-07-17 16:36:50 -0600

    added 'encode' and 'decode' methods to 'Base64'

--- src/main.zig
+++ src/main.zig
@@ -14,7 +14,7 @@ const Base64 = struct {
         };
     }
 
-    pub fn _char_at(self: Base64, index: usize) u8 {
+    fn _char_at(self: Base64, index: usize) u8 {
         return self._table[index];
     }
 
@@ -30,6 +30,78 @@ const Base64 = struct {
         }
 
         return index;
+    }
+
+    fn encode(self: Base64, allocator: std.mem.Allocator, input: []const u8) ![]u8 {
+        if (input.len == 0) {
+            return "";
+        }
+
+        const n_out = try _calc_encode_length(input);
+        var out = try allocator.alloc(u8, n_out);
+        var buf = [3]u8{ 0, 0, 0 };
+        var count: u8 = 0;
+        var iout: u64 = 0;
+
+        for (input, 0..) |_, i| {
+            buf[count] = input[i];
+            count += 1;
+            if (count == 3) {
+                out[iout] = self._char_at(buf[0] >> 2);
+                out[iout + 1] = self._char_at(((buf[0] & 0x03) << 4) + (buf[1] >> 4));
+                out[iout + 2] = self._char_at(((buf[1] & 0x0f) << 2) + (buf[2] >> 6));
+                out[iout + 3] = self._char_at(buf[2] & 0x3f);
+                iout += 4;
+                count = 0;
+            }
+
+            if (count == 1) {
+                out[iout] = self._char_at(buf[0] >> 2);
+                out[iout + 1] = self._char_at((buf[0] & 0x03) << 4);
+                out[iout + 2] = '=';
+                out[iout + 3] = '=';
+            }
+
+            if (count == 2) {
+                out[iout] = self._char_at(buf[0] >> 2);
+                out[iout + 1] = self._char_at(((buf[0] & 0x03) << 4) + (buf[1] >> 4));
+                out[iout + 2] = self._char_at((buf[1] & 0x0f) << 2);
+                out[iout + 3] = '=';
+                iout += 4;
+            }
+
+            return out;
+        }
+    }
+
+    fn decode(self: Base64, allocator: std.mem.Allocator, input: []const u8) ![]u8 {
+        if (input.len == 0) {
+            return "";
+        }
+
+        const n_output = try _calc_decode_length(input);
+        var output = try allocator.alloc(u8, n_output);
+        var count: u8 = 0;
+        var iout: u64 = 0;
+        var buf = [4]u8{ 0, 0, 0, 0 };
+
+        for (0..input.len) |i| {
+            buf[count] = self._char_index(input[i]);
+            count += 1;
+            if (count == 4) {
+                output[iout] = (buf[0] << 2) + (buf[1] >> 4);
+                if (buf[2] != 64) {
+                    output[iout + 1] = (buf[1] << 4) + (buf[2] >> 2);
+                }
+                if (buf[3] != 64) {
+                    output[iout + 2] = (buf[2] << 6) + buf[3];
+                }
+                iout += 3;
+                count = 0;
+            }
+        }
+
+        return output;
     }
 };
 
