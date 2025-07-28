3c4afbbfc0260786872ecadf2ef1e58a19237226
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-07-28 15:42:42 -0600

    added comments for understanding. In process of optimizing 'encode' function.

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,7 @@ pub fn main() !void {}
 const Base64 = struct {
     _table: *const [64]u8,
 
+    // Constructor for 'Base64' struct.
     pub fn init() Base64 {
         const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         const lower = "abcdefghijklmnopqrstuvwxyz";
@@ -14,10 +15,12 @@ const Base64 = struct {
         };
     }
 
+    // Returns char at 'index' argmument in the Base64 table.
     fn _char_at(self: Base64, index: usize) u8 {
         return self._table[index];
     }
 
+    // Returns the index of the 'char' argmument in the Base64 table.
     fn _char_index(self: Base64, char: u8) u8 {
         if (char == '=') {
             return 64;
@@ -43,9 +46,49 @@ const Base64 = struct {
         var count: u8 = 0;
         var iout: u64 = 0;
 
+        // Iterates over indecies of 'input' array.
         for (input, 0..) |_, i| {
+            // For each iteration, the next index of 'input' is added to the next index of 'buf'.
             buf[count] = input[i];
             count += 1;
+
+            // QUESTION: Would it be better to load all 3 elements from the
+            // current group from 'input' at one time, instead of iterating
+            // over each element one-by-one?
+
+            // // My personal optimization (WIP)
+            // // When length of 'buf' is equal to 1
+            // if (count == 1) {
+            //     // Assigns value for current (4 element) output group.
+            //     out[iout] = self._char_at(buf[0] >> 2);
+            // }
+            // out[iout + 1] = self._char_at((buf[0] & 0x03) << 4);
+            // out[iout + 2] = '=';
+            // out[iout + 3] = '=';
+            //
+            // // When length of 'buf' is equal to 2
+            // if (count == 2) {
+            //     // Reassigns values of indexes '1' and '2'
+            //     // of the current (4 element) 6-bit output group
+            //     out[iout + 1] = self._char_at(((buf[0] & 0x03) << 4) + (buf[1] >> 4));
+            //     out[iout + 2] = self._char_at((buf[1] & 0x0f) << 2);
+            // }
+            //
+            // // When length of 'buf' is equal to 3
+            // if (count == 3) {
+            //     // Reassigns values of indexes '2' and '3'
+            //     // of the current (4 element) 6-bit output group
+            //     out[iout + 2] = self._char_at(((buf[1] & 0x0f) << 2) + (buf[2] >> 6));
+            //     out[iout + 3] = self._char_at(buf[2] & 0x3f);
+            //     // Moves output indext to next grouping of 4 6-bit elements
+            //     iout += 4;
+            //     count = 0;
+            //     // QUESTION: Should the buffer also be cleared? Or is this not
+            //     // necessary because each branch only addresses the current or
+            //     // previous elements, but never the next?
+            // }
+
+            // ORIGINAL
             if (count == 3) {
                 out[iout] = self._char_at(buf[0] >> 2);
                 out[iout + 1] = self._char_at(((buf[0] & 0x03) << 4) + (buf[1] >> 4));
@@ -67,11 +110,13 @@ const Base64 = struct {
                 out[iout + 1] = self._char_at(((buf[0] & 0x03) << 4) + (buf[1] >> 4));
                 out[iout + 2] = self._char_at((buf[1] & 0x0f) << 2);
                 out[iout + 3] = '=';
+                // TODO: Figure out why when the count is only '2', the
+                // function moves onto the next output grouping of 4.
                 iout += 4;
             }
-
-            return out;
         }
+
+        return out;
     }
 
     fn decode(self: Base64, allocator: std.mem.Allocator, input: []const u8) ![]u8 {
@@ -85,9 +130,12 @@ const Base64 = struct {
         var iout: u64 = 0;
         var buf = [4]u8{ 0, 0, 0, 0 };
 
+        // Iterate over element of 'input' array.
         for (0..input.len) |i| {
+            // Assign current element of 'input' array to 'buf'.
             buf[count] = self._char_index(input[i]);
             count += 1;
+            // Skip to next iteration if 'buf' does not have 4 current elements
             if (count == 4) {
                 output[iout] = (buf[0] << 2) + (buf[1] >> 4);
                 if (buf[2] != 64) {
